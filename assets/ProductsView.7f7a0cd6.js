fe1cd75366f723cbf2403176d0bbd91a112b6bcf
tksb <tksbsb@gmail.com>
2022-11-05 14:51:16 +0800

    202211051451

---
+++ assets/ProductsView.7f7a0cd6.js
@@ -0,0 +1,1 @@
+import{_ as o,o as a,c as r,a as e,h as t,t as c}from"./index.0ec95a8e.js";const n={},d={class:"about"},i=e("h1",null,"Products",-1);function l(s,u){return a(),r("div",d,[i,e("p",null,[t(" This is the page that opens on "),e("strong",null,"/products/"+c(s.$route.params.id),1),t(" route ")])])}const p=o(n,[["render",l]]);export{p as default};
